23bc73f9885ec3b67911827346768a29c4c17a90
Lautaro Zarate <zlautaro43@gmail.com>
2025-02-25 18:58:34 -0300

    Agregado de links en iconos de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,8 +14,8 @@ return (
             <h3>Síguenos</h3>
             <div className="social-icons">
                 <a href="https://www.instagram.com/seven.studio.design/"><img src="./public/ico-ig.svg" alt="ICONO INSTAGRAM" className="ico-footer"/></a>
-                <a href="#"><img src="./public/ico-lk.svg" alt="ICONO LINKEDIN" className="ico-footer"/></a>
-                <a href="#"><img src="./public/ico-fb.svg" alt="ICONO FACEBOOK" className="ico-footer"/></a>
+                <a href="https://www.linkedin.com/in/seven-studio7/" target="_blank"><img src="./public/ico-lk.svg" alt="ICONO LINKEDIN" className="ico-footer"/></a>
+                <a href="https://www.facebook.com/profile.php?id=61573744315547" target="_blank"><img src="./public/ico-fb.svg" alt="ICONO FACEBOOK" className="ico-footer"/></a>
             </div>
         </div>
     </div>
